ee38e794f672e2c2b166e4b2377cd8d52c43f036
MisaelUrq <misaelurquieta198@hotmail.com>
2019-02-21 16:16:55 -0600

    So this is the commit of the thing that we have view in the class today.

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| WebRoutes
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
@@ -14,3 +14,31 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Examples from class 21/02/201
+
+// Optional values have to be inside {} and hey have to be pass to the
+// lambda as well.
+Route::get('/welcome/{nombre}/{apellido?}',
+           function($nombre, $apellido = null) {
+               // The values are pass with the URL after the page
+               // request 'https://games-forums/welcome/var1/var2'
+
+               // After that we can pass the variables as a hash maep
+               // to the view with the 'with' metod.
+               return view('pages.welcome')->with(['nombre' => $nombre,
+                                                   'apellido' => $apellido]);
+
+               // Or we can pass the variables with the actual name of the variable.
+               // return view('pages.welcome', compac('nombre', 'apellido'));
+           });
+
+
+// This gets inserted after we call php artisian make:auth, aparently
+// what it does is make magic stuff for user registation.
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+// See welcome.blade.php
