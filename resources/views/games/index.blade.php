d6117384eea1b9e6154b2de6c9b75ba767cf7cbc
MisaelUrq <misaelurquieta198@hotmail.com>
2019-03-25 23:12:19 -0600

    Now games are display in tables.

--- resources/views/games/index.blade.php
+++ resources/views/games/index.blade.php
@@ -6,9 +6,27 @@
             <h2>The best place to talk about games.</h2>
         </div>
         @if(count($games) > 0)
-            @foreach($games as $game)
-                <h4>{{$game->name}}</h4>
-            @endforeach
+            <!--TODO(Misael): Maybe tables are a bad choice? Try diferent layouts. To more detail see Bootstrap: Grid -->
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Name</th>
+                        <th scope="col">Director</th>
+                        <th scope="col">Developer</th>
+                        <th scope="col">Publisher</th>
+                    </tr>
+                </thead>
+                @foreach($games as $game)
+                    <tbody>
+                        <tr>
+                            <td>{{$game->name}}</td>
+                            <td>{{$game->director}}</td>
+                            <td>{{$game->developer}}</td>
+                            <td>{{$game->publisher}}</td>
+                        </tr>
+                    </tbody>
+                @endforeach
+            </table>
         @else
             <p>No games have been found.</p>
         @endif
