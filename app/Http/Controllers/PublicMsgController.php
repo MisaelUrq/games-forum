97f9a3df6813843e68722ff9dc28cd2fd5eb3d58
MisaelUrq <misaelurquieta198@hotmail.com>
2019-04-15 14:08:55 -0500

    Now you can reply to message of each post.

--- app/Http/Controllers/PublicMsgController.php
+++ app/Http/Controllers/PublicMsgController.php
@@ -15,19 +15,32 @@ class PublicMsgController extends Controller
             'content' => 'required'
         ]);
 
-        $user = \Auth::user();
-        // TODO(Misael): Make a response to other messages.
-        $msg = new PublicMsg;
-        $msg->content = $request->input('content');
-        $msg->key = $user->name . '_' . time();
-        $msg->sender_id = $user->id;
-        $msg->post_id = $request->input('post_id');;
-        $msg->receiver_id = $request->input('post_id');
-        $msg->likes = 0;
-        $msg->type = 'P';
-        $msg->post_date = date('y-m-d');
-        $msg->save();
-
+        if ($request->input('type_of_reply') === 'M') {
+            $user = \Auth::user();
+            $msg = new PublicMsg;
+            $msg->content = $request->input('content');
+            $msg->key = $user->name . '_' . time();
+            $msg->sender_id = $user->id;
+            $msg->post_id = $request->input('post_id');;
+            $msg->receiver_id = $request->input('receiver_id');;
+            $msg->likes = 0;
+            $msg->type = 'M';
+            $msg->post_date = date('y-m-d');
+            $msg->save();
+        }
+        else {
+            $user = \Auth::user();
+            $msg = new PublicMsg;
+            $msg->content = $request->input('content');
+            $msg->key = $user->name . '_' . time();
+            $msg->sender_id = $user->id;
+            $msg->post_id = $request->input('post_id');;
+            $msg->receiver_id = $request->input('post_id');
+            $msg->likes = 0;
+            $msg->type = 'P';
+            $msg->post_date = date('y-m-d');
+            $msg->save();
+        }
         return redirect('games/' . Blog::where('id', $msg->post_id)->first()->game_id . '/' . $msg->post_id);
     }
 }
--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -1,5 +1,23 @@
 @extends('layouts.app')
 @section('content')
+    <script>
+     function InsertTextArea(key) {
+         var form_block = document.getElementById("reply_to_user-"+key);
+         if (form_block.style.display === "none") {
+             form_block.style.display = "block";
+
+             var button = document.getElementById("button_reply"+key);
+             button.value = "Cancel";
+             button.style.background = "red";
+
+         } else {
+             form_block.style.display = "none";
+             var button = document.getElementById("button_reply"+key);
+             button.value = "Reply";
+             button.style.background = "#3490dc";
+         }
+     }
+    </script>
     <div class="container">
         <div class="container">
             <h1 class="offset-1">{{ $game->name }}</h1>
@@ -12,48 +30,62 @@
         </ul>
     </div>
     @foreach($msgs as $msg)
-    <div class="container">
-        <ul class="list-group m-3">
-            <li class="list-group-item list-group-item-secondary">
-                {{ App\User::where('id', $msg->sender_id)->first()->name }}
-                <small class="offset-10">
-                    {{ $msg->post_date }}
-                </small>
-            </li>
-            <li class="list-group-item">
-                @if($msg->type === 'M')
-                    <ul class="list-group m-2">
-                        <li class="list-group-item ">{{ App\PublicMsg::where('id', $msg->receiver_id)->first()->content }}
-                            <div class="offset-9">
-                                <small>
-                                    {{ App\User::where('id',
-                                                       App\PublicMsg::where('id',
-                                                                            $msg->receiver_id)->first()->sender_id)->first()->name }} -
-                                    {{ App\PublicMsg::where('id', $msg->receiver_id)->first()->post_date }}
-                                </small>
+        <div class="container">
+            <ul class="list-group m-3">
+                <li class="list-group-item list-group-item-secondary">
+                    {{ App\User::where('id', $msg->sender_id)->first()->name }}
+                    <small class="offset-10">
+                        {{ $msg->post_date }}
+                    </small>
+                </li>
+                <li class="list-group-item">
+                    @if($msg->type === 'M')
+                        <ul class="list-group m-2">
+                            <li class="list-group-item ">{{ App\PublicMsg::where('id', $msg->receiver_id)->first()->content }}
+                                <div class="offset-9">
+                                    <small>
+                                        {{ App\User::where('id',
+                                                           App\PublicMsg::where('id',
+                                                                                $msg->receiver_id)->first()->sender_id)->first()->name }} -
+                                        {{ App\PublicMsg::where('id', $msg->receiver_id)->first()->post_date }}
+                                    </small>
+                                </div>
+                            </li>
+                        </ul>
+                    @endif
+                    <p>{{ $msg->content }}</p>
+                    @if(\Auth::user() !== null)
+                        <form id="{{ 'form_div' . $msg->key }}" method="POST" action="{{ route('publicmsg.store') }}">
+                            {{ csrf_field() }}
+                            <div id="{{'reply_to_user-' . $msg->key }}" class="form-group" style="display: none;">
+                                <textarea class="form-control" name="content" rows="3"></textarea>
+                                <input name="type_of_reply" type="hidden" value="M"/>
+                                <input name="post_id" type="hidden" value="{{ $post->id }}"/>
+                                <input name="receiver_id" type="hidden" value="{{ $msg->receiver_id }}"/>
+                                <button class="btn btn-primary mt-1 offset-11">Submit</button>
                             </div>
-                        </li>
-                    </ul>
-                @endif
-                <p>{{ $msg->content }}</p>
-            </li>
-        </ul>
-    </div>
+                        </form>
+                        <input id="{{ 'button_reply' . $msg->key }}" class="btn btn-primary offset-11" name="replyClickMe" type="button" value="Reply" onClick="{{'InsertTextArea("' . $msg->key . '");'}}" />
+                    @endif
+                </li>
+            </ul>
+        </div>
     @endforeach
     <div class="container mt-5">
         @if(\Auth::user() !== null)
             <ul class="list-group">
-                    <li class="list-group-item list-group-item-info">Message</li>
-                    <li class="list-group-item">
-                        <form method="POST" action="{{ route('publicmsg.store') }}">
-                            {{ csrf_field() }}
-                            <div class="form-group">
-                                <textarea class="form-control" name="content" rows="10"></textarea>
-                                <input name="post_id" type="hidden" value="{{ $post->id }}"/>
-                            </div>
-                            <button class="btn btn-primary mt-3 offset-11">Reply</button>
-                        </form>
-                    </li>
+                <li class="list-group-item list-group-item-info">Message</li>
+                <li class="list-group-item">
+                    <form method="POST" action="{{ route('publicmsg.store') }}">
+                        {{ csrf_field() }}
+                        <div class="form-group">
+                            <textarea class="form-control" name="content" rows="10"></textarea>
+                            <input name="type_of_reply" type="hidden" value="P"/>
+                            <input name="post_id" type="hidden" value="{{ $post->id }}"/>
+                        </div>
+                        <button class="btn btn-primary mt-3 offset-11">Reply</button>
+                    </form>
+                </li>
             </ul>
         @else
             <h3>Login to comment.</h3>
