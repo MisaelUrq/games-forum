11185c06aa87f698e9da635659e6808a35781015
MisaelUrq <misaelurquieta198@hotmail.com>
2019-04-15 14:07:47 -0500

    Now when the user logs in he will be in the place he was before.

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -8,15 +8,15 @@ use Illuminate\Foundation\Auth\AuthenticatesUsers;
 class LoginController extends Controller
 {
     /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
+       |--------------------------------------------------------------------------
+       | Login Controller
+       |--------------------------------------------------------------------------
+       |
+       | This controller handles authenticating users for the application and
+       | redirecting them to your home screen. The controller uses a trait
+       | to conveniently provide its functionality to your applications.
+       |
+     */
 
     use AuthenticatesUsers;
 
@@ -36,4 +36,14 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    public function showLoginForm()
+    {
+        if(!session()->has('url.intended'))
+        {
+            session(['url.intended' => url()->previous()]);
+        }
+        return view('auth.login');
+    }
+
 }
