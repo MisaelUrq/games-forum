792fc703c2346e966f7ba9e36679717636a0e8b3
MisaelUrq <misaelurquieta198@hotmail.com>
2019-05-18 21:57:07 -0500

    Added a welcome email and fix a bug where user could pass a already existing user name in the registration form and it will cause the server to crash.

---
+++ resources/views/emails/welcome.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8"/>
+        <title>Document</title>
+    </head>
+    <body>
+        <div class="card">
+            <div class="card-body">
+                <h2 class="card-title">Welcome to Games forums!</h2>
+                <p class="card-text">We hope you enjoy your time.</p>
+            </div>
+        </div>
+    </body>
+</html>
